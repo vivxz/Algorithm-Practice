aad4673a57a4a1eb8d6e49fab6ffa681d7f1747d
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2020-03-02 13:56:25 -0800

    [prog]: Completed subtractProductAndSum algo

---
+++ Easy/subtractProductAndSum.js
@@ -0,0 +1,30 @@
+/*
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+Example 1:
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+
+Example 2:
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+*/
+
+var subtractProductAndSum = function (n) {
+  var nums = n.toString().split('');
+  var product = 1;
+  var sum = 0;
+  for (var i = 0; i < nums.length; i++) {
+    product *= nums[i];
+    sum += Number(nums[i])
+  }
+  return product - sum;
+};
