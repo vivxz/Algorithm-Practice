8bc0240372948402cc654ec9fd7c206e89d79c56
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2020-03-10 19:28:49 -0700

    [prog]: Used a different approach (2 pointer) for reverseString

--- Easy/reverseString.js
+++ Easy/reverseString.js
@@ -16,6 +16,19 @@ Input: ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
 */
 
+// var reverseString = function (s) {
+//   return s.reverse();
+// };
+
 var reverseString = function (s) {
-  return s.reverse();
+  var i = 0;
+  var j = s.length - 1;
+  while (i < j) {
+    var temp = s[i];
+    s[i] = s[j]
+    s[j] = temp
+    i++;
+    j--;
+  }
+  return s;
 };
