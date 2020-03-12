089c63f1b60d8ae681a645bb5ed61acabc7a8a41
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2020-03-11 21:15:16 -0700

    [prog]: Completed addDigits algo

---
+++ Easy/addDigits.js
@@ -0,0 +1,21 @@
+/*
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+            Since 2 has only one digit, return it.
+*/
+
+var addDigits = function(num) {
+  if (num > 9) {
+    var sum = 0;
+    num = num.toString();
+    for (var i = 0; i < num.length; i++){
+      sum += Number(num[i]);
+    }
+    return addDigits(sum);
+  } 
+  return num;
+};
