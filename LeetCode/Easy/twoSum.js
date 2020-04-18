b7138d0b411f5744a419f2bb36b1e469fd4ffb58
Vivian Quan <viviannquan@gmail.com>
2020-04-17 22:43:58 -0700

    [prog]: Redid to return indexes instead of values

--- LeetCode/Easy/twoSum.js
+++ LeetCode/Easy/twoSum.js
@@ -13,9 +13,9 @@ return [0, 1].
 
 var twoSum = function (nums, target) {
   for (var i = 0; i < nums.length; i++) {
-    for (var j = 0; j < nums.length; j++) {
+    for (var j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
-        return [nums[i], nums[j]];
+        return [i, j];
       }
     }
   }
