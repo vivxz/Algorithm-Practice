c98cbfdf5ba875f81cce9e4e3faf1335315b9614
Vivian Quan <viviannquan@gmail.com>
2020-04-20 22:15:29 -0700

    [prog]: Refactored code for faster runtime

--- LeetCode/Easy/isAnagram.js
+++ LeetCode/Easy/isAnagram.js
@@ -17,22 +17,54 @@ What if the inputs contain unicode characters? How would you adapt your solution
 */
 
 var isAnagram = function (s, t) {
-  if (s === "" && t === "") {
+  // if (s === "" && t === "") {
+  //   return true;
+  // }
+
+  // s = s.split('').sort()
+  // t = t.split('').sort()
+
+  // console.log(s)
+  // console.log(t)
+
+  // console.log(s[1] === t[1])
+
+  // for (var i = 0; i < s.length; i++) {
+  //   if (s[i] !== t[i] || s.length !== t.length) {
+  //     return false;
+  //   } 
+  // }
+  // return true
+
+  var container = {};
+
+  if (s.length !== t.length){
+    return false;
+  } else if (s === "" && t === "") {
     return true;
   }
 
-  s = s.split('').sort()
-  t = t.split('').sort()
-
-  console.log(s)
-  console.log(t)
-
-  console.log(s[1] === t[1])
-
-  for (var i = 0; i < s.length; i++) {
-    if (s[i] !== t[i] || s.length !== t.length) {
-      return false;
-    } 
+  for (var i = 0; i < s.length; i++){
+    if (container[s[i]]){
+      container[s[i]]++;
+    } else {
+      container[s[i]] = 1;
+    }
   }
-  return true
+
+  for (var j = 0; j < t.length; j++){
+    if (container[t[j]]){
+      container[t[j]]--;
+    } else {
+      return false;
+    }
+  }
+
+  for (var key in container){
+    if (container[key] !== 0){
+      return false; 
+    } else {
+      return true;
+    }
+  }
 };
