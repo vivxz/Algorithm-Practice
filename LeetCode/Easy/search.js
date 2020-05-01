036a17f2399f4311637b6036edcbbbba52899091
Vivian Quan <viviannquan@gmail.com>
2020-04-30 23:56:13 -0700

    [prog]: Solved binary search array algo

---
+++ LeetCode/Easy/search.js
@@ -0,0 +1,30 @@
+/*
+Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+*/
+
+var search = function(nums, target) {
+  var low = 0;
+  var high = nums.length - 1;
+  while (low <= high){
+    var middle = Math.floor((low + high)/2);
+    if (nums[middle] < target){
+      low = middle + 1;
+    } else if (nums[middle] > target){
+      high = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+  return -1;
+};
