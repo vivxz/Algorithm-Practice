68a4c2ec0b248f03ed6f6863b27f315d0f5a8338
Vivian Quan <viviannquan@gmail.com>
2020-07-30 21:17:43 -0700

    [prog]: Solved LCA algo

---
+++ LeetCode/Easy/lowestCommonAncestor.js
@@ -0,0 +1,32 @@
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+Example 1:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+
+Example 2:
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+ */
+
+var lowestCommonAncestor = function(root, p, q) {
+  if (root === null) return null
+  if (q.val === root.val || p.val === root.val) {
+      return root
+  }
+      
+  const treeLeft = lowestCommonAncestor(root.left, p, q);
+  const treeRight = lowestCommonAncestor(root.right, p, q);
+  
+  if (treeLeft === null) return treeRight;
+  if (treeRight === null) return treeLeft;
+  
+  return root
+};
